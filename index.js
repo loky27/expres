6a738bd587ada4dd595fe0d86f2277ea69d0b1ca
Oscar Islas <oislasreyes@gmail.com>
2021-01-05 20:17:31 -0600

    Leyendo el archivo de contacto

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 let http = require("http");
+let fs = require("fs");
 
 http.createServer((request, response) => {
+    response.setHeader("Content-Type", "text/html; charset=utf-8");
     if (request.url === "/contacto") {
-      response.setHeader("Content-Type", "text/html; charset=utf-8");
-      response.write("<h1>Página de contacto</h1>");
-      response.end();
-    } else if (request.url === "/") {
-      response.setHeader("Content-Type", "text/html; charset=utf-8");
+      fs.readFile("./contacto.html", (error, content) => {
+        if(!error){
+            response.write(content);
+            response.end();
+        }else{
+            response.write("<h1>404</h1>");
+            response.end();
+        }
+      });
+    } else if (request.url === "/") {     
       response.write("<h1>Página de inicio</h1>");
       response.end();
+    } else if (request.url === "/usuarios") {  
+        response.setHeader("Content-Type", "application/json; charset=utf-8");   
+        let users = [{
+            nombre: "Javier",
+            email: "javier09@gmail.com"
+        }]
+        response.write(JSON.stringify(users));
+        response.end();
     } else {
-      response.setHeader("Content-Type", "text/html; charset=utf-8");
       response.write("<h1>404</h1>");
       response.end();
     }
