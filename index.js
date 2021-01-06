0a30ab018126d9e6879a9ac6d07b074c802645c3
Oscar Islas <oislasreyes@gmail.com>
2021-01-05 19:56:21 -0600

    servidor web sin morgan

--- index.js
+++ index.js
@@ -1,25 +1,17 @@
 let http = require("http");
-let morgan = require("morgan");
-
-let logger = morgan('dev');
 
 http.createServer((request, response) => {
-    logger(request, response, (err) => {
-        if (err) return done(err)
-
-        if(request.url === "/contacto"){
-            response.setHeader("Content-Type", "text/html; charset=utf-8");
-            response.write("<h1>Página de contacto</h1>");
-            response.end();
-        }else if(request.url === "/"){
-            response.setHeader("Content-Type", "text/html; charset=utf-8");
-            response.write("<h1>Página de inicio</h1>");
-            response.end();
-        }else{
-            response.setHeader("Content-Type", "text/html; charset=utf-8");
-            response.write("<h1>404</h1>");
-            response.end();
-        }
-        
-    });
+    if (request.url === "/contacto") {
+      response.setHeader("Content-Type", "text/html; charset=utf-8");
+      response.write("<h1>Página de contacto</h1>");
+      response.end();
+    } else if (request.url === "/") {
+      response.setHeader("Content-Type", "text/html; charset=utf-8");
+      response.write("<h1>Página de inicio</h1>");
+      response.end();
+    } else {
+      response.setHeader("Content-Type", "text/html; charset=utf-8");
+      response.write("<h1>404</h1>");
+      response.end();
+    }
 }).listen(8080);
