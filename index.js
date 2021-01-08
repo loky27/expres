758455673c474920ac935d45f3e091cdda94308b
Oscar Islas <oislasreyes@gmail.com>
2021-01-07 20:15:41 -0600

    Manejando las peticiones de tipo GET con express

--- index.js
+++ index.js
@@ -7,9 +7,6 @@ let app = express();
 //Middleware
 app.use(express.static("public"));
 app.use(express.static("assets"));
-//Procesar los datos que son enviados a través del formulario
-//Y va a colocar los datos transformados sobre request.body
-app.use(express.urlencoded({extended: false})); 
 
 //app.get -> get
 app.get("/", (request, response) => {
@@ -26,15 +23,6 @@ app.get("/nosotros", (request, response) => {
 
 app.get("/proyectos", (request, response) => {
     response.sendFile(path.join(__dirname, "projects.html"));
-});
-
-app.post("/usuarios", (request, response) => {    
-    fs.writeFile("usuarios_db.txt", JSON.stringify(request.body), (error) => {
-        if(error){
-            console.log(error);
-        }
-        response.redirect("/");
-    });
 });
 
 //app.use -> get, post, delete, put
