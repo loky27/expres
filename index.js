037eec8b24789b6ec7f48ddb5b4de0f64ade8de5
Oscar Islas <oislasreyes@gmail.com>
2021-01-07 20:01:39 -0600

    servidor web con express

--- index.js
+++ index.js
@@ -1,94 +1,47 @@
-let http = require("http");
+let express = require("express");
 let fs = require("fs");
-let mime = require("mime");
+let path = require("path");
 
-http.createServer((request, response) => {
-    if(request.method === "GET"){
-        switch(request.url){
-            case "/contacto":
-                readFile("/contact.html", response);
-                break;
-            case "/":
-                readFile("/index.html", response);
-                break;
-            case "/nosotros":
-                readFile("/about.html", response);
-                break;
-            case "/proyectos":
-                readFile("/projects.html", response);
-                break;
-            case "/favicon.ico":
-                response.setHeader("Content-Type", "image/x-icon");
-                readFile("/favicon.ico", response);
-                break;
-            default:
-                readFile(request.url, response);
-                break;
+let app = express();
+
+//Middleware
+app.use(express.static("public"));
+app.use(express.static("assets"));
+//Procesar los datos que son enviados a través del formulario
+//Y va a colocar los datos transformados sobre request.body
+app.use(express.urlencoded({extended: false})); 
+
+//app.get -> get
+app.get("/", (request, response) => {
+    response.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/contacto", (request, response) => {
+    response.sendFile(path.join(__dirname, "contact.html"));
+});
+
+app.get("/nosotros", (request, response) => {
+    response.sendFile(path.join(__dirname, "about.html"));
+});
+
+app.get("/proyectos", (request, response) => {
+    response.sendFile(path.join(__dirname, "projects.html"));
+});
+
+app.post("/usuarios", (request, response) => {    
+    fs.writeFile("usuarios_db.txt", JSON.stringify(request.body), (error) => {
+        if(error){
+            console.log(error);
         }
-    }else if(request.method === "POST"){
-        switch(request.url){
-            case "/usuarios":
-                agregarUsuario(request);
-                break;
-            default:
-                readFile(request.url, response);
-                break;
-        }
-
-    }
-}).listen(8080);
-
-
-const readFile = (url, response) => {
-    let urlF = __dirname + url;
-    console.log(__dirname, url);
-    fs.readFile(urlF, (error, content) => {
-        if(!error){
-            //mime.getType(urlF) -> nos regresaría el mime type dependiendo de la ruta
-            //styles.css -> text/css
-            //contact.html -> text/html
-            response.setHeader("Content-Type", mime.getType(urlF));
-            response.end(content);
-        }else{
-            response.writeHead(404);
-            response.end("<h1>404</h1>")
-        }
-    })
-}
-
-const agregarUsuario = (request) => {
-    let data = '';
-
-    //Cuando se estén recibiendo datos
-    request.on('data', chunk => {
-        data += chunk;
+        response.redirect("/");
     });
-    
-    //Cuando se terminen de procesar los datos
-    request.on('end', () => {
-        let datos = data.toString();
-        console.log("Fin del stream");
-        //fs.writeFile
-        //1° argumento -> la ruta del archivo en el que queremos escribir 
-        //Se creará el archivo si no existe en la ruta especificada 
-        //2° argumento -> El contenido que queremos escribir
-        //3° argumento -> función de callback que nos "notificará" en caso de que haya
-        // un error al escribir en el archivo
-        console.log(datos.split("&"));
-        let user = {
-            name: datos.split("&")[0].split("=")[1],
-            lastname: datos.split("&")[1].split("=")[1],
-            email: datos.split("&")[2].split("=")[1],
-            password: datos.split("&")[3].split("=")[1]
-        }
-        fs.writeFile("usuarios2.txt", JSON.stringify(user), (error) => {
-            if(error){
-                console.log(error);
-            }
-        });
-    });
+});
 
-    request.on('error', error => {
-        console.log(error);
-    })
-}
+//app.use -> get, post, delete, put
+app.use((request, response) => {
+    response.sendFile(path.join(__dirname, "404.html"));
+});
+
+app.listen(8080, () => {
+    console.log("Servidor iniciado en el puerto 8080");
+});
