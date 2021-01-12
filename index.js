7482a4de33227211f25f595e16c799fd2c853c7c
loky27 <hef6666qgmail.com>
2021-01-12 04:57:06 -0600

    tareea

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let express = require("express");
 let fs = require("fs");
+const { request } = require("https");
 let path = require("path");
 
 let app = express();
@@ -7,7 +8,7 @@ let app = express();
 //Middleware
 app.use(express.static("public"));
 app.use(express.static("assets"));
-
+app.use(express.urlencoded({extended:false}))
 //app.get -> get
 app.get("/", (request, response) => {
     response.sendFile(path.join(__dirname, "index.html"));
@@ -24,6 +25,16 @@ app.get("/nosotros", (request, response) => {
 app.get("/proyectos", (request, response) => {
     response.sendFile(path.join(__dirname, "projects.html"));
 });
+app.post("/usuarios",(request,resposnse)=>{
+    fs.writeFile("usuarios_db.tx",JSON.stringify(request.body),(error)=>{
+        if(error){
+            console.log(error)
+        }
+        else{
+            resposnse.redirect("/")
+        }
+    })
+})
 
 //app.use -> get, post, delete, put
 app.use((request, response) => {
